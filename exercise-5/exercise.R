8f58834f3b447a7f075af2b1bbdc9802c00c2ec4
Justin Sim <justis5@uw.edu>
2018-04-20 15:25:21 -0700

    modify assignment.R files with work completed for exercises 5 through 8

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -9,22 +9,33 @@ library(dplyr)
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
-
+dep_delay_by_month <- group_by(flights, month) %>% 
+  summarize(average = mean(dep_delay, na.rm = TRUE))
 
 # Which month had the greatest average departure delay?
-
+dep_delay_by_month %>% 
+  filter(average == max(average)) %>% 
+  select(month)
 
 # If your above data frame contains just two columns (e.g., "month", and "delay" in that order), you can create
 # a scatterplot by passing that data frame to the 'plot()' function
-
+plot(dep_delay_by_month)
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
-
-
+arr_delay_by_month <- group_by(flights, dest) %>% 
+  summarize(avg_delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  arrange(-avg_delay)
+head(arr_delay_by_month)
 # You can look up these airports in the `airports` data frame!
-
+filter(airports, faa == "MSN")
+filter(airports, faa == "TUL")
 
 # Which city was flown to with the highest average speed?
-
+city_highest_avg_sped <- flights %>% 
+  mutate(speed = distance / air_time * 60) %>% 
+  group_by(dest) %>% 
+  summarize(avg_sped = mean(speed, na.rm = TRUE)) %>% 
+  filter(avg_sped == max(avg_sped, na.rm = TRUE)) %>% 
+  select(dest)
--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,36 +1,49 @@
 # Exercise 8: exploring data sets
 
 # Load the dplyr library
-
+library(dplyr)
 
 # Read in the data (from `data/pupulitzer-circulation-data.csv`). Remember to 
 # not treat strings as factors!
-
+data <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
 
 # View in the data set. Start to understand what the data set contains
-
+View(data)
 
 # Print out the names of the columns for reference
-
+colnames(data)
 
 # Use the 'str()' function to also see what types of values are contained in 
 # each column (you're looking at the second column after the `:`)
 # Did any value type surprise you? Why do you think they are that type?
-
+str(data) # Why is daily circulation in characters?
 
 # Add a column to the data frame called 'Pulitzer.Prize.Change` that contains 
 # the difference in the number of times each paper was a winner or finalist 
 # (hereafter "winner") during 2004-2014 and during 1990-2003
-
+data <- data %>% 
+  mutate(Pulitzer.Prize.Change = Pulitzer.Prize.Winners.and.Finalists..2004.2014 
+         - Pulitzer.Prize.Winners.and.Finalists..1990.2003)
 
 # What was the name of the publication that has the most winners between 
 # 2004-2014?
+most_winners <- data %>% 
+  filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 ==
+           max(Pulitzer.Prize.Winners.and.Finalists..2004.2014)) %>% 
+  select(Newspaper)
 
 
 # Which publication with at least 5 winners between 2004-2014 had the biggest
 # decrease(negative) in daily circulation numbers?
-
-
+biggest_decrease <- data %>% 
+  filter(Pulitzer.Prize.Winners.and.Finalists..2004.2014 >= 5) %>% 
+  filter(Change.in.Daily.Circulation..2004.2013 == min(Change.in.Daily.Circulation..2004.2013)) %>% 
+  select(Newspaper)
+  
 # An important part about being a data scientist is asking questions. 
 # Write a question you may be interested in about this data set, and then use  
 # dplyr to figure out the answer!
+# Which publication has had the greatest increase in the number of Pulitzers?
+most_improved <- data %>% 
+  filter(Pulitzer.Prize.Change == max(Pulitzer.Prize.Change)) %>% 
+  select(Newspaper, Pulitzer.Prize.Change)
